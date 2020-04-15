9ad71e5e07ab60b2a151f74a5bf864b3474911dd
Stevan Mihajlovic <stevan-mihajlovic@outlook.com>
2020-04-15 18:00:43 +0200

    script for full vocabularies implemented script for cutting them based on word frequency implemented 3 examples of full vocabularies and 1 example of cut added

---
+++ constants.py
@@ -0,0 +1,6 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATASETS_PATH = os.path.join(ROOT_DIR, "twitter-datasets")
+VOCABULARIES_FULL_PATH = os.path.join(ROOT_DIR, "vocabularies", "full")
+VOCABULARIES_CUT_PATH = os.path.join(ROOT_DIR, "vocabularies", "cut")
--- vocabularies/cuttings/cut-vocab-frequency.py
+++ vocabularies/cuttings/cut-vocab-frequency.py
@@ -2,3 +2,23 @@
 #        -f <frequency number> (minimum frequency for a word so that it is kept)
 
 # OUTPUT: cut/cut-vocab-frequency<frequency number>.pkl
+# TYPE: list(tuple)<(word, frequency)>
+
+import pickle
+from argparse import ArgumentParser
+import constants
+import os
+
+parser = ArgumentParser()
+parser.add_argument("-v", choices=["test", "train-short", "train-full", "test-and-train-short", "test-and-train-full"])
+parser.add_argument("-f", type=int)
+args = parser.parse_args()
+
+with open(os.path.join(constants.VOCABULARIES_FULL_PATH, f"{args.v}.pkl"), "rb") as inputfile:
+    words = pickle.load(inputfile)
+
+# Select all words occurring at least n times.
+cut_vocab = [word for (word, count) in words if count > args.f]
+
+with open(os.path.join(constants.VOCABULARIES_CUT_PATH, f"cut-vocab-{args.v}-frequency-{args.f}.pkl"), "wb") as outputfile:
+    pickle.dump(cut_vocab, outputfile)
